34959c715ae8bdfc9c25005931856f3585523a13
asiagovincent11@gmail.com <asiagovincent11@gmail.com>
2023-10-18 12:57:59 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+
+
+// Define the currentUser variable
+const currentUser = 'Grace Hopper';
+
+// Create the welcomeMessage using template literal
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create the excitedWelcomeMessage by converting welcomeMessage to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create the shortGreeting by taking the first character of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+// Test the code
+console.log(welcomeMessage); // Should display "Welcome to Flatbook, Grace Hopper!"
+console.log(excitedWelcomeMessage); // Should display "WELCOME TO FLATBOOK, GRACE HOPPER!"
+console.log(shortGreeting); // Should display "Welcome, G!"
